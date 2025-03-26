9aba710d132caf293df8a4a2ed79bd9409b20379
unknown <qimihazri@gmail.com>
2025-03-26 10:00:14 +0100

    changes on bluetooth name

--- src/MIDI_Interfaces/BLEMIDI/ESP32/midi-init.c
+++ src/MIDI_Interfaces/BLEMIDI/ESP32/midi-init.c
@@ -21,7 +21,7 @@
 
 #include <assert.h>
 
-const char *midi_ble_name = "Control Surface (BLE)";
+const char *midi_ble_name = "midimap (BLE)";
 
 void set_midi_ble_name(const char *name) { midi_ble_name = name; }
 
